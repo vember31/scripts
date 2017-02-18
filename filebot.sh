b01c35d0b7f8aba93fa88d4059259aa13e209f96
vember31 <prestigioustennis@gmail.com>
2017-02-18 01:29:19 -0600

    Update filebot.sh

--- filebot.sh
+++ filebot.sh
@@ -10,7 +10,7 @@ outputDir=$"/home/(user)/Documents/external_hdd/Organized_Media" #ensure no back
 paplay /home/duke/Downloads/Turn.wav #play a sound to signify the download is complete
 
 #call filebot w/update sonarr/radarr script within
-#{info.id} returns the tvdb or tmdb, depending on whether it's a TV show or a movie
+# {info.id} returns the tvdb or tmdb, depending on whether it's a TV show or a movie
 filebot -script fn:amc --output $outputDir --action duplicate --conflict skip -non-strict --log-file amc.log --def "exec=/home/(user)/Documents/update_sonarr_and_radarr.sh \"{info.id}\"" excludeList=.excludes unsorted=y music=y artwork=y xbmc=$kodiClients pushbullet=$pushbulletAPI "ut_dir=$ARG_PATH/$ARG_NAME" "ut_kind=multi" "ut_title=$ARG_NAME"
 
 
