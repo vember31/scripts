39e81f9b15a1de72a5b842a4d4103f8d018c26e7
vember31 <prestigioustennis@gmail.com>
2017-02-18 10:11:18 -0600

    Update filebot.sh

--- filebot.sh
+++ filebot.sh
@@ -2,17 +2,20 @@
 ARG_ID="$1"
 ARG_NAME="$2"
 ARG_PATH="$3"
+username=$"username"
 pushbulletAPI=$"insert pushbullet API here"
 kodiClients=$"192.168.1.124,192.168.1.184" #the two pcs - comma delimited
-outputDir=$"/home/(user)/Documents/external_hdd/Organized_Media" #ensure no backslash @ the end!
+outputDir=$"/home/$username/Documents/external_hdd/Organized_Media" #ensure no backslash @ the end!
 
 #----------Script--------#
-paplay /home/(user)/Downloads/Turn.wav #play a sound to signify the download is complete
+paplay /home/$username/Downloads/Turn.wav #play a sound to signify the download is complete
 
 #call filebot w/update sonarr/radarr script within
 # {info.id} returns the tvdb or tmdb, depending on whether it's a TV show or a movie
-filebot -script fn:amc --output $outputDir --action duplicate --conflict skip -non-strict --log-file amc.log --def seriesFormat="TV Shows/{n}/Season {s.pad(2)}/{n} - {s00e00} - {t}  [{source}-{vf}]" movieFormat="Movies/{ny}/{ny} [{source}-{vf}]" "exec=/home/(user)/Documents/update_sonarr_and_radarr.sh \"{info.id}\"" excludeList=.excludes unsorted=y music=y artwork=y xbmc=$kodiClients pushbullet=$pushbulletAPI "ut_dir=$ARG_PATH/$ARG_NAME" "ut_kind=multi" "ut_title=$ARG_NAME"
+filebot -script fn:amc --output $outputDir --action duplicate --conflict skip -non-strict --log-file amc.log --def seriesFormat="TV Shows/{n}/Season {s.pad(2)}/{n} - {s00e00} - {t}  [{source}-{vf}]" movieFormat="Movies/{ny}/{ny} [{source}-{vf}]" "exec=/home/$username/Documents/update_sonarr_and_radarr.sh \"{info.id}\"" excludeList=.excludes unsorted=y music=y artwork=y xbmc=$kodiClients pushbullet=$pushbulletAPI "ut_dir=$ARG_PATH/$ARG_NAME" "ut_kind=multi" "ut_title=$ARG_NAME"
 
 # Notes
 #seriesFormat="TV Shows/{n}/Season {s.pad(2)}/{n} - {s00e00} - {t}  [{source}-{vf}]"
+# Ex: /TV Shows/Season 05/Suits - S05E05 - Whatever [HDTV-720p]
 #movieFormat="Movies/{ny}/{ny} [{source}-{vf}]"
+# Ex: /Movies/Avatar (2009)/Avatar (2009) [Bluray-1080p]
