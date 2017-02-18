527c3201e34c2a6a112cf57348162663a8d59d27
vember31 <prestigioustennis@gmail.com>
2017-02-18 10:18:32 -0600

    Update filebot.sh

--- filebot.sh
+++ filebot.sh
@@ -12,7 +12,7 @@ paplay /home/$username/Downloads/Turn.wav #play a sound to signify the download 
 
 #call filebot w/update sonarr/radarr script within
 # {info.id} returns the tvdb or tmdb, depending on whether it's a TV show or a movie
-filebot -script fn:amc --output $outputDir --action duplicate --conflict skip -non-strict --log-file amc.log --def seriesFormat="TV Shows/{n}/Season {s.pad(2)}/{n} - {s00e00} - {t}  [{source}-{vf}]" movieFormat="Movies/{ny}/{ny} [{source}-{vf}]" "exec=/home/$username/Documents/update_sonarr_and_radarr.sh \"{info.id}\"" excludeList=.excludes unsorted=y music=y artwork=y xbmc=$kodiClients pushbullet=$pushbulletAPI "ut_dir=$ARG_PATH/$ARG_NAME" "ut_kind=multi" "ut_title=$ARG_NAME"
+filebot -script fn:amc --output $outputDir --action duplicate --conflict skip -non-strict --log-file amc.log --def seriesFormat="TV Shows/{n}/Season {s.pad(2)}/{n} - {s00e00} - {t}  [{source}-{vf}]" movieFormat="Movies/{ny}/{ny} [{source}-{vf}]" "exec=/home/\$username\/Documents/update_sonarr_and_radarr.sh \"{info.id}\"" excludeList=.excludes unsorted=y music=y artwork=y xbmc=$kodiClients pushbullet=$pushbulletAPI "ut_dir=$ARG_PATH/$ARG_NAME" "ut_kind=multi" "ut_title=$ARG_NAME"
 
 # Notes
 #seriesFormat="TV Shows/{n}/Season {s.pad(2)}/{n} - {s00e00} - {t}  [{source}-{vf}]"
