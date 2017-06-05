16ecf3e680ccb276e75289a887cc68a45b956ba1
vember31 <prestigioustennis@gmail.com>
2017-06-05 09:53:18 -0500

    Removing parameterization

--- update_sonarr_and_radarr.sh
+++ update_sonarr_and_radarr.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-infoId=$1 #this is being passed in by the filebot.sh script.  It is the TVDB ID (if a tv show) or the TMDB (if a movie).  
-sonarrAPIkey=$"API Key"
-radarrAPIkey=$"API Key"
+infoId=$1 #this is being passed in by the filebot.sh script.  It is the TVDB ID (if a tv show) or the TMDB (if a movie). 
+#sonarrAPIkey=$"API Key"
+#radarrAPIkey=$"API Key"
 
 
 # Below, we must first get 'seriesId' or 'movieId', which are the Sonarr/Radarr internal IDs from the TVDB or TMDB ID passed in by filebot.
 # The second line is responsible for calling the API to manually update the specific series/movie.
 # ------------- Sonarr ----------------
-seriesId=$(curl -s http://localhost:8989/api/series -H 'X-Api-Key: $sonarrAPIkey' --compressed | jq -s '.[]| .[] | select(.tvdbId=='$infoId') | .id ')
+seriesId=$(curl -s http://localhost:8989/api/series -H 'X-Api-Key: [Sonarr API Key]' --compressed | jq -s '.[]| .[] | select(.tvdbId=='$infoId') | .id ')
 
-curl -s  http://localhost:8989/api/command -H 'X-Api-Key: $sonarrAPIkey' --data-binary '{ "name": "rescanSeries", "seriesId" : "'$seriesId'" }'
+curl -s  http://localhost:8989/api/command -H 'X-Api-Key: [Sonarr API Key]' --data-binary '{ "name": "rescanSeries", "seriesId" : "'$seriesId'" }'
 
 # ------------- Radarr -----------------
-movieId=$(curl -s http://localhost:7878/api/movie -H 'X-Api-Key: $radarrAPIkey' --compressed | jq -s '.[]| .[] | select(.tmdbId=='$infoId') | .id ')
+movieId=$(curl -s http://localhost:7878/api/movie -H 'X-Api-Key: [Radarr API Key]' --compressed | jq -s '.[]| .[] | select(.tmdbId=='$infoId') | .id ')
 
-curl -s  http://localhost:7878/api/command -H 'X-Api-Key: $radarrAPIkey' --data-binary '{ "name": "RescanMovie", "movieId" : "'$movieId'" }'
+curl -s  http://localhost:7878/api/command -H 'X-Api-Key: [Radarr API Key]' --data-binary '{ "name": "RescanMovie", "movieId" : "'$movieId'" }'
