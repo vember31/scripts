7347b77a33c127caeb2108569a6cf346e4fd1c42
vember31 <prestigioustennis@gmail.com>
2019-10-06 16:42:57 -0500

    Update jackett_reinstall.sh

--- jackett_reinstall.sh
+++ jackett_reinstall.sh
@@ -2,7 +2,7 @@
 
 # Run script as root to reinstall jackett, if it fails during an auto-update.
 
-sudo rm Jackett.Binaries.LinuxAMDx64.tar.gz
+sudo rm /opt/Jackett.Binaries.LinuxAMDx64.tar.gz
 sudo service jackett stop
 echo "Stopping Jackett service."
 sleep 5
@@ -11,12 +11,10 @@ echo "Current Jackett directory removed."
 release=$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep "title>Release" | cut -d " " -f 4)
 sudo wget -q https://github.com/Jackett/Jackett/releases/download/$release/Jackett.Binaries.LinuxAMDx64.tar.gz
 echo "Downloaded latest Jackett release."
-sudo tar -xf Jackett*
+sudo tar -xf /opt/Jackett*
 echo "Extracted latest Jackett Release."
 sudo chown -R duke:duke /opt/Jackett/
-cd /opt/Jackett/
-sudo ./install_service_systemd.sh
-cd /opt/
-sudo rm Jackett.Binaries.LinuxAMDx64.tar.gz
+sudo /opt/Jackett/install_service_systemd.sh
+sudo rm /opt/Jackett.Binaries.LinuxAMDx64.tar.gz
 echo "Removed Jackett installer tar."
 echo "Script complete."
