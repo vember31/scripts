9dacf6e210145e80300e1ec737f145d944d6ed33
vember31 <prestigioustennis@gmail.com>
2023-10-04 21:17:32 -0500

    remove local files older than 60 days

--- vaultwarden_backup.sh
+++ vaultwarden_backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-zip -r "vaultwarden_backup_$(date +"%m-%d-%Y").zip" /opt/vaultwarden/data/
-rclone copy "vaultwarden_backup_$(date +"%m-%d-%Y").zip" gdrive:Vaultwarden
+find /opt/vaultwarden/backup/* -mtime +60 -type f -exec rm {} \;
+zip -r "/opt/vaultwarden/backup/vaultwarden_backup_$(date +"%m-%d-%Y").zip" /opt/vaultwarden/data/
+rclone sync "/opt/vaultwarden/backup/vaultwarden_backup_$(date +"%m-%d-%Y").zip" gdrive:Vaultwarden
 rclone delete gdrive:Vaultwarden --min-age 30d
